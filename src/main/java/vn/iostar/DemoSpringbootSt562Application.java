e85a2f971c97b6f0f21b1d9b59dd45125a17837a
Phan Duy Khương <handuykhuong05012005@gmail.com>
2025-09-19 23:33:55 +0700

    Nộp BT 7

---
+++ src/main/java/vn/iostar/DemoSpringbootSt562Application.java
@@ -0,0 +1,11 @@
+package vn.iostar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootSt562Application {  
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringbootSt562Application.class, args);
+    }
+}
